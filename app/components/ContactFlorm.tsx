c1812bb4c7b1f4de75fb6c02c6ef6dc981b7e640
Erik Nilsson <nilsson.erik.k@gmail.com>
2025-01-09 16:09:38 +0100

    Using NextUI Form

--- app/components/ContactFlorm.tsx
+++ app/components/ContactFlorm.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
+import { Form } from "@nextui-org/form";
 import Link from "next/link";
 import { Input } from "@nextui-org/input";
 import React, { useState } from "react";
 
 export default function ContactForm() {
   const [hasSentMessage, setHasSentMessage] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log("submitted");
+    setIsLoading(true);
     const formData = new FormData(e.target as HTMLFormElement);
     const data = Object.fromEntries(formData.entries());
 
@@ -23,13 +24,13 @@ export default function ContactForm() {
       method: "POST",
       body: JSON.stringify(data),
     }).then((res) => {
-      console.log("🚀 ~ handleSubmit ~ res:", res);
       if (res.ok) {
         setHasSentMessage(true);
       } else {
         setHasSentMessage(false);
       }
     });
+    setIsLoading(false);
   }
 
   return (
@@ -38,56 +39,40 @@ export default function ContactForm() {
         <div className="flex flex-col items-center justify-center gap-4">
           <h4>Thanks for reaching out!</h4>
           <p>I will get back to you as soon as possible.</p>
-          <Link href="/" className="p-2 bg-gray-400 text-white rounded-md">
-            Back
+          <Link href="/">
+            <Button>Back</Button>
           </Link>
         </div>
       ) : (
-        <>
+        <div className="flex flex-col items-center justify-center gap-4 w-full">
           <h2 className="text-2xl font-bold">Contact me</h2>
-          <form
-            className="flex flex-col items-center justify-center gap-4"
-            onSubmit={handleSubmit}
-          >
-            <div className="w-full flex flex-col gap-2">
-              <label htmlFor="name">Name</label>
-              <input
-                className="p-2 border border-gray-300 rounded-md"
-                type="text"
-                name="name"
-                placeholder="Name"
-              />
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <Input label="Email" type="email" />
-              {/* <label htmlFor="email">Email</label>
-              <input
-                className="p-2 border border-gray-300 rounded-md"
-                type="email"
-                name="email"
-                placeholder="Email"
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                title="Please enter a valid email address"
-              /> */}
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <label htmlFor="message">Message</label>
-              <input
-                className="p-2 border border-gray-300 rounded-md"
-                type="text"
-                name="message"
-              />
-            </div>
-            <div className="flex gap-2 w-full justify-end">
-              <Link href="/">
-                <Button>Back</Button>
-              </Link>
-              <Button color="primary" type="submit">
-                Submit
-              </Button>
-            </div>
-          </form>
-        </>
+          <div className="w-full flex flex-col gap-8">
+            <Form
+              onSubmit={handleSubmit}
+              className="w-full flex flex-col gap-4"
+            >
+              <div className="flex flex-col gap-2 w-full">
+                <Input label="Name" type="text" name="name" />
+                <Input label="Email" type="email" name="email" />
+                <Input
+                  label="Message"
+                  type="text"
+                  name="message"
+                  className="w-full"
+                />
+              </div>
+
+              <div className="flex gap-2 w-full justify-end">
+                <Link href="/">
+                  <Button>Back</Button>
+                </Link>
+                <Button color="primary" type="submit" isLoading={isLoading}>
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
       )}
     </div>
   );
